a96262137b68080b2001f0a1ba4dfbdb1f32b8ee
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-13 18:21:39 +0000

    create fixture list view

--- predictor/views.py
+++ predictor/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Fixture
 
 # Create your views here.
 def home(request):
     return render(request, "predictor/home.html")
+
+def fixture_list(request):
+    fixtures = Fixture.objects.all()
+
+    context = {
+        "fixtures": fixtures
+    }
+
+    return render(request, "predictor/fixture_list.html", context)
