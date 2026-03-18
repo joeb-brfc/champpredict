7492733205d6791cf958bde46d81fdbd2d42225e
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-03-18 10:40:53 +0000

    add matchweek validation and prediction improvements

--- champpredict/settings.py
+++ champpredict/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
